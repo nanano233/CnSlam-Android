9bbd13721fd2bdc819c49544fc2c72b61dec6c39
nanchen <2507312857@qq.com>
2026-04-17 15:31:28 +0800

    修复IMU逻辑

--- app/src/main/cpp/import/ORBSLAM/importORBSLAM.cpp
+++ app/src/main/cpp/import/ORBSLAM/importORBSLAM.cpp
@@ -20,6 +20,8 @@
 
 #include <opencv2/core.hpp>
 
+#include <exception>
+
 static const char* TAG = "ORBSLAM";
 
 extern "C"
@@ -104,13 +106,22 @@ Java_cn_koistudio_hitomi_module_OrbSlam_SystemMono_nCreateSystemMono(JNIEnv *env
     try
     {
         // 调用修改后的 System 构造函数
+//        system = new ORB_SLAM3::System(filename_voctxt_str, filename_setting_str,
+//                                       ORB_SLAM3::System::MONOCULAR, false, 0, "Android",
+//                                       string(run_type_str), string(server_ip_str), string(server_port_str));
         system = new ORB_SLAM3::System(filename_voctxt_str, filename_setting_str,
-                                       ORB_SLAM3::System::MONOCULAR, false, 0, "Android",
+                                       ORB_SLAM3::System::IMU_MONOCULAR, false, 0, "Android",
                                        string(run_type_str), string(server_ip_str), string(server_port_str));
     }
-    catch(int err)
-    {
-        __android_log_print(ANDROID_LOG_INFO, TAG, "catch error %d",err);
+    catch(const std::exception& e) {
+        // 把 OpenCV 的格式错误打印在 Logcat 里
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "C++ FATAL EXCEPTION: %s", e.what());
+    }
+    catch(int err) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "catch error %d", err);
+    }
+    catch(...) {
+        __android_log_print(ANDROID_LOG_ERROR, TAG, "Unknown C++ crash");
     }
 
 
